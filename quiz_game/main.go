aaff09f746f98093e4265afe3dec152a6145a573
Shivam Kumar <shivamk1717@gmail.com>
2024-10-11 15:37:35 +0530

    part 1 done

--- quiz_game/main.go
+++ quiz_game/main.go
@@ -7,6 +7,21 @@ import (
 	"os"
 )
 
+type problem struct {
+	q string
+	a string
+}
+
+func parseCsvRecords(records [][]string) []problem {
+	problemList := make([]problem, len(records))
+	for i, line := range records {
+		problemList[i] = problem{
+			q: line[0],
+			a: line[1],
+		}
+	}
+	return problemList
+}
 func main() {
 	csvFile := flag.String("csv", "problems.csv", "csv file of question,answer file")
 	flag.Parse()
@@ -15,10 +30,25 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	csvReader := csv.NewReader(file)
 	records, err := csvReader.ReadAll()
 	if err != nil {
 		panic(err)
 	}
+
 	fmt.Println("files data", records)
+
+	problems := parseCsvRecords(records)
+	counter := 0
+	for _, eachProblem := range problems {
+		fmt.Println("Question: ", eachProblem.q)
+		var answer string
+		fmt.Scanf("%s", &answer)
+		if answer == eachProblem.a {
+			fmt.Println("correct")
+			counter += 1
+		}
+	}
+	fmt.Printf("You got %d from %d\n", counter, len(problems))
 }
